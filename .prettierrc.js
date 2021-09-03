176f1d3ca0c07b5f116f5816bf527ce4dc7b5a11
汪洋 <iwangyang@qq.com>
2021-09-03 21:56:29 +0800

    prettierrc to english

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  // 一个tab代表几个空格数 默认为 2
+  // A tab represents several spaces Default to two
   tabWidth: 2,
-  // 是否使用tab进行缩进 默认为 false，表示用空格进行缩减
+  // Indent with tab Default to False, indicating space reduction
   useTabs: false,
-  // 字符串是否使用单引号 默认为 false，使用双引号
+  // Does the string use single quotes Default to False, use double quotes
   singleQuote: true,
-  // 行位是否使用分号 默认为 true
+  // Whether semicolon is used in row position Default to true
   semi: false,
-  // 是否使用尾逗号 有三个可选值 "< none | es5 | all >"
+  // Use trailing comma There are three optional values "< none | es5 | all >"
   trailingComma: 'all',
-  // 对象大括号直接是否有空格 默认为 true 效果：{ foo: bar }
+  // Object braces directly check whether there are spaces Default to true effect:{ foo: bar }
   bracketSpacing: true,
 }
